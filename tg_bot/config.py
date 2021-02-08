70837a6882cf673662c3442853ce96ffc12bc32b
sum897 <78513094+sum897@users.noreply.github.com>
2021-02-09 03:43:31 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -13,13 +13,13 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "--"
-    OWNER_ID = "Ha Ha"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "legendx22"
+    OWNER_ID = "1608740498"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "LEAVEIY"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    GBAN_LOGS = -1001400676390 #Channel ID here with -
+    GBAN_LOGS = -1001331605998 #Channel ID here with -
     LOAD = []
     NO_LOAD = ['translation', 'rss']   
     WEBHOOK = False
